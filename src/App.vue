953d547fcec3c84cea4852b6975ded98d1d3ef30
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-06 20:35:27 +0600

    custom event with data
    passing event name and data in parameter of ()

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay"  @end="endGame" />
+  <p v-if="showResults">reaction time: {{ score }} ms</p>
  </template>
 
 <script>
@@ -11,7 +12,9 @@ export default {
   data(){
     return{
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   components: {
@@ -21,6 +24,12 @@ export default {
     start(){
       this.delay= 2000+ Math.random() * 5000;
       this.isPlaying= true;
+      this.showResults= false
+    },
+    endGame(reactionTime){
+      this.score= reactionTime;
+      this.isPlaying= false;
+      this.showResults= true;
     }
   }
 }
